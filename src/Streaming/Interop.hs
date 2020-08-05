c7d626f193a2c4114fad407ea74960f1e14bdd22
Divesh Otwani <divesh.otwani@gmail.com>
2020-08-04 21:32:02 -0400

    Processors

--- src/Streaming/Interop.hs
+++ src/Streaming/Interop.hs
@@ -1,13 +1,29 @@
 {-# LANGUAGE LinearTypes #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RebindableSyntax #-}
+{-# LANGUAGE RecordWildCards #-}
 
--- | This module contains functions for interoperating with other 
+-- | This module contains functions for interoperating with other
 -- streaming libraries.
 module Streaming.Interop
-  (
-  {-
-  , reread
-  -}
+  ( reread
   ) where
 
+import Streaming.Type
+import Streaming.Produce
+import Data.Unrestricted.Linear
+import Prelude.Linear (($), (&))
+import Prelude (Maybe(..))
+import Control.Monad.Linear.Builder (BuilderType(..), monadBuilder)
+
+reread :: CMonad m =>
+  (s -> m (Unrestricted (Maybe a))) -> s -> Stream (Of a) m ()
+reread f s = Effect $ do
+  Unrestricted maybeA <- f s
+  case maybeA of
+    Nothing -> return $ Return ()
+    Just a -> return $ (yield a >> reread f s)
+  where
+    Builder{..} = monadBuilder
 
 
